eb03f5fd61a79d96280a6ef7b410f803f4d9d6f1
Lasse Pouru <lasse.pouru@koha-suomi.fi>
2023-12-11 09:41:59 +0200

    Do not try to connect to RabbitMQ

--- misc/workers/background_jobs_worker.pl
+++ misc/workers/background_jobs_worker.pl
@@ -25,8 +25,7 @@ background_jobs_worker.pl - Worker script that will process background jobs
 
 =head1 DESCRIPTION
 
-This script will connect to the Stomp server (RabbitMQ) and subscribe to the queues passed in parameter (or the 'default' queue),
-or if a Stomp server is not active it will poll the database every 10s for new jobs in the passed queue.
+This script will poll the database every 10s for new jobs in the passed queue (or the 'default' queue).
 
 You can specify some queues only (using --queue, which is repeatable) if you want to run several workers that will handle their own jobs.
 
@@ -91,112 +90,30 @@ unless (@queues) {
     push @queues, 'default';
 }
 
-my $conn;
-try {
-    $conn = Koha::BackgroundJob->connect;
-} catch {
-    warn sprintf "Cannot connect to the message broker, the jobs will be processed anyway (%s)", $_;
-};
-
 my $pm = Parallel::ForkManager->new($max_processes);
 
-if ( $conn ) {
-    # FIXME cf note in Koha::BackgroundJob about $namespace
-    my $namespace = C4::Context->config('memcached_namespace');
-    for my $queue (@queues) {
-        $conn->subscribe(
-            {
-                destination      => sprintf( "/queue/%s-%s", $namespace, $queue ),
-                ack              => 'client',
-                'prefetch-count' => 1,
-            }
-        );
-    }
-}
 while (1) {
-    if ( $conn ) {
-        my $frame = $conn->receive_frame( { timeout => $mq_timeout } );
-        if ( !defined $frame ) {
-            # timeout or connection issue?
-            $pm->reap_finished_children;
-            next;    # will reconnect automatically
-        }
-
+    my $jobs = Koha::BackgroundJobs->search({ status => 'new', queue => \@queues });
+    while ( my $job = $jobs->next ) {
         my $args = try {
-            my $body = $frame->body;
-            decode_json($body); # TODO Should this be from_json? Check utf8 flag.
+            $job->json->decode($job->data);
         } catch {
-            Koha::Logger->get({ interface => 'worker' })->warn(sprintf "Frame not processed - %s", $_);
+            Koha::Logger->get({ interface => 'worker' })->warn(sprintf "Cannot decode data for job id=%s", $job->id);
+            $job->status('failed')->store;
             return;
         };
 
-        unless ( $args ) {
-            Koha::Logger->get({ interface => 'worker' })->warn(sprintf "Frame does not have correct args, ignoring it");
-            $conn->nack( { frame => $frame, requeue => 'false' } );
-            next;
-        }
-
-        my $job = Koha::BackgroundJobs->find( $args->{job_id} );
-
-        if ( $job && $job->status ne 'new' ) {
-            Koha::Logger->get( { interface => 'worker' } )
-                ->warn( sprintf "Job %s has wrong status %s", $args->{job_id}, $job->status );
-
-            # nack without requeue, we do not want to process this frame again
-            $conn->nack( { frame => $frame, requeue => 'false' } );
-            next;
-        }
-
-        unless ($job) {
-            $not_found_retries->{ $args->{job_id} } //= 0;
-            if ( ++$not_found_retries->{ $args->{job_id} } >= $max_retries ) {
-                Koha::Logger->get( { interface => 'worker' } )
-                    ->warn( sprintf "Job %s not found, no more retry", $args->{job_id} );
-
-                # nack without requeue, we do not want to process this frame again
-                $conn->nack( { frame => $frame, requeue => 'false' } );
-                next;
-            }
-
-            Koha::Logger->get( { interface => 'worker' } )
-                ->debug( sprintf "Job %s not found, will retry later", $args->{job_id} );
-
-            # nack to force requeue
-            $conn->nack( { frame => $frame, requeue => 'true' } );
-            Time::HiRes::sleep(0.5);
-            next;
-        }
-        $conn->ack( { frame => $frame } );
+        next unless $args;
 
         $pm->start and next;
         srand();    # ensure each child process begins with a new seed
-        process_job( $job, $args );
+        process_job( $job, { job_id => $job->id, %$args } );
         $pm->finish;
 
-    } else {
-        my $jobs = Koha::BackgroundJobs->search({ status => 'new', queue => \@queues });
-        while ( my $job = $jobs->next ) {
-            my $args = try {
-                $job->json->decode($job->data);
-            } catch {
-                Koha::Logger->get({ interface => 'worker' })->warn(sprintf "Cannot decode data for job id=%s", $job->id);
-                $job->status('failed')->store;
-                return;
-            };
-
-            next unless $args;
-
-            $pm->start and next;
-            srand();    # ensure each child process begins with a new seed
-            process_job( $job, { job_id => $job->id, %$args } );
-            $pm->finish;
-
-        }
-        $pm->reap_finished_children;
-        sleep 10;
     }
+    $pm->reap_finished_children;
+    sleep 10;
 }
-$conn->disconnect;
 $pm->wait_all_children;
 
 sub process_job {
