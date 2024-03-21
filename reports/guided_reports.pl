378f37728be15a5bf9601c37201fa4d32fbd63e0
Pasi Kallinen <pasi.kallinen@koha-suomi.fi>
2024-03-21 15:00:03 +0200

    Add Finna-compatible JSON report export format
    Needs "finnaprefix" system preference.

--- reports/guided_reports.pl
+++ reports/guided_reports.pl
@@ -635,6 +635,14 @@ elsif ($op eq 'export'){
             while ( my $row = $sth->fetchrow_arrayref() ) {
                 $content .= join( "\t", $scrubber->scrub(@$row) ) . "\n";
             }
+        } elsif ($format eq 'finna') {
+            $type = 'application/json';
+            my @biblios;
+            while (my $row = $sth->fetchrow_hashref()) {
+                push(@biblios, $row->{'biblionumber'}) if ($row->{'biblionumber'});
+            }
+            $reportfilename = $reportfilename.".json";
+            $content = finnajson(\@biblios, $reportname);
         } else {
             if ( $format eq 'csv' ) {
                 my $delimiter = C4::Context->csv_delimiter;
@@ -1067,6 +1075,33 @@ if ( $op eq 'list' || $op eq 'convert') {
         has_obsolete_reports    => $has_obsolete_reports,
     );
 }
+
+#################################################
+# pick shelves data from array-reference and make json string (finna format)
+sub  finnajson {
+  my @args=@_;
+  my $biblios_ref=$args[0];
+  my $shelfname=$args[1];
+  my $finnaprefix = C4::Context->preference('finnaprefix');
+  my $json_string="{\n \"lists\":[{\n\"description\": null,\n\"public\": 0,\n\"records\":[\n";
+  my $i=0;
+  my $count=scalar @$biblios_ref;
+     while($i < $count)
+     {
+       if($i >0)
+       {
+         $json_string.=",";
+       }
+
+       $json_string.="{\n\"id\":\"".$finnaprefix.$biblios_ref->[$i]."\",\n";
+       $json_string.="\"notes\":null,\n\"order\":$i,\n\"source\":\"Solr\",\n\"tags\":[]\n}";
+       $i++;
+     }
+     $json_string.="\n],\n\"title\":\"".$shelfname."\"\n\}\n],\"searches\":[]\n}";
+     $json_string = Encode::encode('UTF-8', $json_string);
+     return($json_string);
+}
+
 # pass $sth, get back an array of names for the column headers
 sub header_cell_values {
     my $sth = shift or return ();
