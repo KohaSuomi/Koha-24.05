KohaSuomi/Koha

KD-197: Use field 942$m as default field in signum plugins
If field 942$m is not found use one of the main entry
fields (1XX). Otherwise use field 245$a.

cataloguing/value_builder/084a_mainheading_branchloc_genre_signum_builder.pl
@@ -71,6 +71,8 @@ function Click$function_name(event) {
         // Do main heading
         // Actually we should also follow the bypass indicators here
 
+        var marc942f = resp.f942m;
+
         var marc100a = resp.f100a;
         var marc110a = resp.f110a;
         var marc111a = resp.f111a;
@@ -81,7 +83,9 @@ function Click$function_name(event) {
         // Second indicator is 'bypass'
         var marc245a = resp.f245a;
 
-        if (marc100a) {
+        if (marc942m) {
+            var mainHeading = marc942m;
+        } else if (marc100a) {
             var mainHeading = marc100a;
         } else if (marc110a) {
             var mainHeading = marc110a;

KohaSuomi/Koha-24.05

cataloguing/value_builder/084a_mainheading_branchloc_genre_signum_builder.pl
@@ -71,6 +71,8 @@ function Click$function_name(event) {
         // Do main heading
         // Actually we should also follow the bypass indicators here
 
+        var marc942m = resp.f942m;
+
         var marc100a = resp.f100a;
         var marc110a = resp.f110a;
         var marc111a = resp.f111a;
@@ -81,7 +83,9 @@ function Click$function_name(event) {
         // Second indicator is 'bypass'
         var marc245a = resp.f245a;
 
-        if (marc100a) {
+        if (marc942m) {
+            var mainHeading = marc942m;
+        } else if (marc100a) {
             var mainHeading = marc100a;
         } else if (marc110a) {
             var mainHeading = marc110a;