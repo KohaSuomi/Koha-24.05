KohaSuomi/Koha

Bug 22322: Self check-in without log in
One should be able to return loans via SCO without
login required. This patch adds check in box where
patrons can check in their loans with barcode.

To test:
1. Set "SelfCheckoutByLogin" as "cardnumber".
2. Find patron with loan or add one.
3. Go to /cgi-bin/koha/sco/sco-main.pl
4. Return loan by passing items barcode
=> confirm loans is returned succesfully

Sponsored-by: Koha-Suomi Oy

opac/sco/sco-main.pl
@@ -33,10 +33,12 @@ use Modern::Perl;
 use CGI qw ( -utf8 );
 
 use C4::Auth qw( in_iprange get_template_and_user checkpw );
-use C4::Circulation qw( barcodedecode AddReturn CanBookBeIssued AddIssue CanBookBeRenewed AddRenewal );
-use C4::Reserves;
+use C4::Circulation qw( barcodedecode AddReturn CanBookBeIssued AddIssue CanBookBeRenewed AddRenewal updateWrongTransfer );
+use C4::Reserves qw( ModReserveAffect );
 use C4::Output qw( output_html_with_http_headers );
 use C4::Members;
+use C4::Biblio qw( GetBiblioData );
+use C4::Items qw( ModItemTransfer );
 use Koha::DateUtils qw( dt_from_string );
 use Koha::Acquisition::Currencies;
 use Koha::Items;
@@ -45,6 +47,7 @@ use Koha::Patron::Images;
 use Koha::Patron::Messages;
 use Koha::Plugins;
 use Koha::Token;
+use Koha::Calendar;
 
 my $query = CGI->new;
 
@@ -73,9 +76,11 @@ $query->param(-name=>'sco_user_login',-values=>[1]);
 my ( $template, $loggedinuser, $cookie ) = get_template_and_user(
     {
         template_name   => "sco/sco-main.tt",
+        authnotrequired => 0,
         flagsrequired   => { self_check => "self_checkout_module" },
         query           => $query,
         type            => "opac",
+        debug => 1,
     }
 );
 
@@ -93,13 +98,15 @@ if (defined C4::Context->preference('SCOAllowCheckin')) {
 }
 
 my $issuerid = $loggedinuser;
-my ($op, $patronlogin, $patronpw, $barcode, $confirmed, $newissues) = (
+my ($op, $patronlogin, $patronpw, $barcode, $confirmed, $newissues, $uibarcode, $checkinmessage) = (
     $query->param("op")         || '',
     $query->param("patronlogin")|| '',
     $query->param("patronpw")   || '',
     $query->param("barcode")    || '',
     $query->param("confirmed")  || '',
     $query->param("newissues")  || '',
+    $query->param("uibarcode")  || '',
+    $query->param("checkinmessage") || undef,
 );
 
 my $jwt = $query->cookie('JWT');
@@ -114,6 +121,18 @@ $barcode = barcodedecode( $barcode ) if $barcode;
 my @newissueslist = split /,/, $newissues;
 my $issuenoconfirm = 1; #don't need to confirm on issue.
 my $issuer   = Koha::Patrons->find( $issuerid )->unblessed;
+
+my $checkinitem;
+my $checkinbranchcode;
+my $userenv = C4::Context->userenv;
+my $userenv_branch = $userenv->{'branch'} // '';
+my $daysmode = Koha::CirculationRules->get_effective_daysmode(
+    {
+        branchcode   => $userenv_branch,
+    }
+);
+my $calendar    = Koha::Calendar->new( branchcode => $userenv_branch, days_mode => $daysmode );
+my $today       = DateTime->now( time_zone => C4::Context->tz());
 
 my $patronid = $jwt ? Koha::Token->new->decode_jwt({ token => $jwt }) : undef;
 unless ( $patronid ) {
@@ -138,7 +157,7 @@ my $branch = $issuer->{branchcode};
 my $confirm_required = 0;
 my $return_only = 0;
 
-if ( $patron && $op eq "returnbook" && $allowselfcheckreturns ) {
+if ( ( $op eq "checkin" && $uibarcode ) || $patron && $op eq "returnbook" && $allowselfcheckreturns ) {
     my $success = 1;
 
 
@@ -151,16 +170,66 @@ if ( $patron && $op eq "returnbook" && $allowselfcheckreturns ) {
         }
     }
 
-    if ($success) {
+    if ($success && ( $op ne "checkin" && !$uibarcode )) {
         # Patron cannot checkin an item they don't own
         $success = 0
           unless $patron->checkouts->find( { itemnumber => $item->itemnumber } );
     }
 
-    if ( $success ) {
-        ($success) = AddReturn( $barcode, $branch )
+    $uibarcode =~ s/^\s+|\s+$//g;
+    $checkinitem = Koha::Items->find({ barcode => $uibarcode });
+    if( !$checkinitem ){
+        $checkinmessage = "Item not found.";
+    }
+    my $tobranch = $checkinitem->homebranch;
+
+    my ($return_success,$messages,$issueinformation,$borrower) = AddReturn($uibarcode,$branch,undef,$today) unless !$success;
+
+    my $needstransfer = $messages->{'NeedsTransfer'};
+    if($messages->{'ResFound'}) {
+        my $reserve = $messages->{'ResFound'};
+        my $reserve_id = $reserve->{'reserve_id'};
+        my $resborrower = $reserve->{'borrowernumber'};
+        my $diffBranchReturned = $reserve->{'branchcode'};
+        my $itemnumber = $checkinitem->itemnumber;
+        my $diffBranchSend = ($branch ne $diffBranchReturned) ? $diffBranchReturned : undef;
+        if($diffBranchSend) {
+            ModReserveAffect( $itemnumber, $resborrower, $diffBranchSend, $reserve_id);
+            ModItemTransfer($itemnumber,$branch,$diffBranchReturned, 'ResFound');
+        }
+        else {
+            my $settransit = C4::Context->preference('RequireSCCheckInBeforeNotifyingPickups') ? 1 : 0;
+            ModReserveAffect( $itemnumber, $resborrower, $settransit, $reserve_id);
+        }
+        $borrower = Koha::Patrons->find({ cardnumber => $patronid });
+    }
+    else {
+         if($needstransfer) {
+             ModItemTransfer($checkinitem->itemnumber, $branch, $tobranch, 'NeedsTransfer');
+         }
     }
 
+    if($messages->{'WrongTransfer'}) {
+       updateWrongTransfer($checkinitem->itemnumber,$tobranch,$branch);
+    }
+
+    my $biblio = Koha::Biblios->find({ biblionumber => $checkinitem->biblionumber });
+    if ( $biblio && ( $success || $return_success )) {
+        $checkinmessage = "Returned ".$biblio->title;
+    }
+
+    if ( $issuer ) {
+        $checkinbranchcode = $issuer->{branchcode};
+        #home branch of item
+        if ($checkinbranchcode) {
+            my $checkinlibrary = Koha::Libraries->find( $checkinbranchcode );
+            $checkinmessage.=", ".$checkinlibrary->branchname;
+        }
+    }
+
+    $template->param(checkinmessage => $checkinmessage || undef);
+    $template->param(SelfCheckTimeout => 10000); #don't show returns long
+    $template->param(uibarcode => $uibarcode);
     $template->param( returned => $success );
 }
 elsif ( $patron && ( $op eq 'checkout' ) ) {

KohaSuomi/Koha-24.05

opac/sco/sco-main.pl
@@ -33,10 +33,11 @@ use Modern::Perl;
 use CGI qw ( -utf8 );
 
 use C4::Auth        qw( in_iprange get_template_and_user checkpw );
-use C4::Circulation qw( barcodedecode AddReturn CanBookBeIssued AddIssue CanBookBeRenewed AddRenewal );
-use C4::Reserves;
+use C4::Circulation qw( barcodedecode AddReturn CanBookBeIssued AddIssue CanBookBeRenewed AddRenewal updateWrongTransfer );
+use C4::Reserves qw ( ModReserveAffect );
 use C4::Output qw( output_html_with_http_headers );
 use C4::Members;
+use C4::Items qw( ModItemTransfer );
 use Koha::DateUtils qw( dt_from_string );
 use Koha::Acquisition::Currencies;
 use Koha::Items;
@@ -68,6 +69,7 @@ $query->param( -name => 'sco_user_login', -values => [1] );
 my ( $template, $loggedinuser, $cookie ) = get_template_and_user(
     {
         template_name => "sco/sco-main.tt",
+        authnotrequired => 0,
         flagsrequired => { self_check => "self_checkout_module" },
         query         => $query,
         type          => "opac",
@@ -88,14 +90,16 @@ if ( defined C4::Context->preference('SCOAllowCheckin') ) {
 }
 
 my $issuerid = $loggedinuser;
-my ( $op, $patronlogin, $patronpw, $barcodestr, $confirmed, $newissues, $load_checkouts ) = (
-    $query->param("op")             || '',
-    $query->param("patronlogin")    || '',
-    $query->param("patronpw")       || '',
-    $query->param("barcode")        || '',
-    $query->param("confirmed")      || '',
-    $query->param("newissues")      || '',
-    $query->param("load_checkouts") || '',
+my ( $op, $patronlogin, $patronpw, $barcodestr, $confirmed, $newissues, $load_checkouts, $sco_entry_barcode, $returned_item ) = (
+    $query->param("op")                || '',
+    $query->param("patronlogin")       || '',
+    $query->param("patronpw")          || '',
+    $query->param("barcode")           || '',
+    $query->param("confirmed")         || '',
+    $query->param("newissues")         || '',
+    $query->param("load_checkouts")    || '',
+    $query->param("sco_entry_barcode") || '',
+    $query->param("returned_item")     || undef,
 );
 
 my $jwt = $query->cookie('JWT');
@@ -108,8 +112,8 @@ if ( $op eq "logout" ) {
 }
 
 my $barcodes = [];
-if ($barcodestr) {
-    push @$barcodes, split( /\s\n/, $barcodestr );
+if ($barcodestr || $sco_entry_barcode) {
+    push @$barcodes, split( /\s\n/, $barcodestr ? $barcodestr : $sco_entry_barcode );
     $barcodes = [ map { $_ =~ /^\s*$/ ? () : barcodedecode($_) } @$barcodes ];
 }
 
@@ -149,11 +153,19 @@ if ( $patron ) {
     }
 }
 
-if ( $patron && $op eq "cud-returnbook" && $allowselfcheckreturns ) {
+if ( ( $op eq "cud-sco_entry_checkin" ) || ( $patron && $op eq "cud-returnbook" && $allowselfcheckreturns ) ) {
     my $success = 1;
 
     foreach my $barcode (@$barcodes) {
         my $item = Koha::Items->find( { barcode => $barcode } );
+
+        my $title          = $item ? Koha::Biblios->find({ biblionumber => $item->biblionumber })->title : undef;
+        my $checkinlibrary = $issuer && $branch ? Koha::Libraries->find( $branch )->branchname : undef;
+
+        if( !$item ){
+            $success = 0;
+        }
+
         if ( $success && C4::Context->preference("CircConfirmItemParts") ) {
             if ( defined($item)
                 && $item->materials )
@@ -162,20 +174,51 @@ if ( $patron && $op eq "cud-returnbook" && $allowselfcheckreturns ) {
             }
         }
 
-        if ($success) {
+        if ($success && ( $op ne "cud-sco_entry_checkin" )) {
 
             # Patron cannot checkin an item they don't own
             $success = 0
                 unless $patron->checkouts->find( { itemnumber => $item->itemnumber } );
         }
 
-        if ($success) {
-            ($success) = AddReturn( $barcode, $branch );
+        if ( $success ) {
+
+            my $to_branch = $item->homebranch;
+            my $messages;
+
+            ( $success, $messages ) = AddReturn( $barcode, $branch );
+
+            if( $messages->{'ResFound'} ) {
+                my $reserve          = $messages->{'ResFound'};
+                my $reserve_id       = $reserve->{'reserve_id'};
+                my $resborrower      = $reserve->{'borrowernumber'};
+                my $resbranch        = $reserve->{'branchcode'};
+                my $itemnumber       = $item->itemnumber;
+                my $diff_branch_send = ($branch ne $resbranch) ? $resbranch : undef;
+
+                if( $diff_branch_send ) {
+                    ModReserveAffect( $itemnumber, $resborrower, $diff_branch_send, $reserve_id );
+                    ModItemTransfer( $itemnumber, $branch, $resbranch, 'ResFound' );
+                } else {
+                    my $set_transit = C4::Context->preference('RequireSCCheckInBeforeNotifyingPickups');
+                    ModReserveAffect( $itemnumber, $resborrower, $set_transit, $reserve_id);
+                }
+            } elsif ( $messages->{'NeedsTransfer'} ) {
+                    ModItemTransfer( $item->itemnumber, $branch, $to_branch, 'NeedsTransfer' );
+            }
+
+            if( $messages->{'WrongTransfer'} ) {
+                updateWrongTransfer( $item->itemnumber, $to_branch, $branch );
+            }
+
         }
 
         $template->param(
+            SelfCheckTimeout => 10000, #don't show returns long
             returned => $success,
-            barcode  => $barcode
+            barcode  => $barcode,
+            title => $title,
+            library => $checkinlibrary
         );
     }    # foreach barcode in barcodes
 