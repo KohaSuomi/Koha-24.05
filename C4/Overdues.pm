e3a76cae7efb8988c2cd7b162e7543fecd0b18ec
lmstrand <lmstrand@gmail.com>
2025-02-06 14:50:19 +0200

    G984 Add branchcode for OVERDUE accountlines

--- C4/Overdues.pm
+++ C4/Overdues.pm
@@ -540,6 +540,7 @@ sub UpdateFine {
     my $borrowernumber = $params->{borrowernumber};
     my $amount         = $params->{amount};
     my $due            = $params->{due} // q{};
+    my $overdue_branch = $params->{branchcode};
 
     unless ( $issue_id ) {
         carp("No issue_id passed in!");
@@ -627,6 +628,7 @@ sub UpdateFine {
                     type        => 'OVERDUE',
                     item_id     => $itemnum,
                     issue_id    => $issue_id,
+                    library_id  => $overdue_branch,
                 }
             );
         }
