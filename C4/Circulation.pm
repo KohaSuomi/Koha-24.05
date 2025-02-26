c9bbfb2869a03325c5f3f6651a47342b0908329d
lmstrand <lmstrand@gmail.com>
2025-02-26 16:36:49 +0200

    G1613 add processing for no_block issues' existing reserves by SIP config

--- C4/Circulation.pm
+++ C4/Circulation.pm
@@ -4299,6 +4299,8 @@ sub ProcessOfflineIssue {
 
     my $patron = Koha::Patrons->find( { cardnumber => $operation->{cardnumber} } );
     $patron ||= Koha::Patrons->find( { userid => $operation->{cardnumber} } );
+    
+    my $no_block_prevreserve_status = $operation->{no_block_prevreserve_status};
 
     if ( $patron ) {
         my $item = Koha::Items->find({ barcode => $operation->{barcode} });
@@ -4319,7 +4321,7 @@ sub ProcessOfflineIssue {
             $patron,
             $operation->{barcode},
             $operation->{due_date},
-            undef,
+            $no_block_prevreserve_status,
             $operation->{timestamp},
             undef,
         );
--- C4/SIP/ILS/Transaction/Checkout.pm
+++ C4/SIP/ILS/Transaction/Checkout.pm
@@ -50,6 +50,7 @@ sub do_checkout {
         my $overridden_duedate;    # usually passed as undef to AddIssue
         my $prevcheckout_block_checkout         = $account->{prevcheckout_block_checkout};
         my $allow_additional_materials_checkout = $account->{allow_additional_materials_checkout};
+        my $no_block_prevreserve_status         = $account->{no_block_prevreserve_status};
         my ( $issuingimpossible, $needsconfirmation, $messages ) = _can_we_issue( $patron, $barcode, 0 );
 
     if ( $no_block_due_date ) {
@@ -155,6 +156,7 @@ sub do_checkout {
             barcode    => $barcode,
             due_date   => $no_block_due_date,
             timestamp  => dt_from_string,
+            no_block_prevreserve_status => $no_block_prevreserve_status || undef,
         });
         $self->{due} = $self->duedatefromissue($checkout, $itemnumber);
     } else {
