1ddfedc41d87eed226c70d1511d2b9a9f836afc3
Johanna Räisä <johanna.raisa@gmail.com>
2025-03-11 14:00:00 +0200

    Add hold pickup shelves API

---
+++ Koha/REST/V1/HoldPickupShelves.pm
@@ -0,0 +1,138 @@
+package Koha::REST::V1::HoldPickupShelves;
+
+# Copyright 2023 Theke Solutions
+#
+# This file is part of Koha.
+#
+# Koha is free software; you can redistribute it and/or modify it
+# under the terms of the GNU General Public License as published by
+# the Free Software Foundation; either version 3 of the License, or
+# (at your option) any later version.
+#
+# Koha is distributed in the hope that it will be useful, but
+# WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with Koha; if not, see <http://www.gnu.org/licenses>.
+
+use Modern::Perl;
+
+use Mojo::Base 'Mojolicious::Controller';
+
+use Koha::HoldPickupShelves;
+
+use Scalar::Util qw( blessed );
+use Try::Tiny    qw( catch try );
+
+=head1 API
+
+=head2 Methods
+
+=head3 list
+
+=cut
+
+sub list {
+    my $c = shift->openapi->valid_input or return;
+
+    return try {
+        return $c->render(
+            status  => 200,
+            openapi => $c->objects->search( Koha::HoldPickupShelves->new )
+        );
+    } catch {
+        $c->unhandled_exception($_);
+    };
+}
+
+=head3 get
+
+=cut
+
+sub get {
+    my $c = shift->openapi->valid_input or return;
+
+    return try {
+        my $hold_pickup_shelf = $c->objects->find( Koha::HoldPickupShelves->new, $c->param('hold_pickup_shelf_id') );
+        return $c->render_resource_not_found("Record source")
+            unless $hold_pickup_shelf;
+
+        return $c->render( status => 200, openapi => $hold_pickup_shelf );
+    } catch {
+        $c->unhandled_exception($_);
+    };
+}
+
+=head3 add
+
+=cut
+
+sub add {
+    my $c = shift->openapi->valid_input or return;
+
+    return try {
+        my $hold_pickup_shelf = Koha::HoldPickupShelf->new_from_api( $c->req->json );
+        $hold_pickup_shelf->store;
+        $c->res->headers->location( $c->req->url->to_string . '/' . $hold_pickup_shelf->hold_pickup_shelf_id );
+        return $c->render(
+            status  => 201,
+            openapi => $c->objects->to_api($hold_pickup_shelf)
+        );
+    } catch {
+        $c->unhandled_exception($_);
+    };
+}
+
+=head3 update
+
+=cut
+
+sub update {
+    my $c = shift->openapi->valid_input or return;
+
+    my $hold_pickup_shelf = $c->objects->find_rs( Koha::HoldPickupShelves->new, $c->param('hold_pickup_shelf_id') );
+
+    return $c->render_resource_not_found("Hold pickup shelf")
+        unless $hold_pickup_shelf;
+
+    return try {
+        $hold_pickup_shelf->set_from_api( $c->req->json )->store;
+        $hold_pickup_shelf->discard_changes;
+        return $c->render( status => 200, openapi => $c->objects->to_api($hold_pickup_shelf) );
+    } catch {
+        $c->unhandled_exception($_);
+    };
+}
+
+=head3 delete
+
+=cut
+
+sub delete {
+    my $c = shift->openapi->valid_input or return;
+
+    my $hold_pickup_shelf = $c->objects->find_rs( Koha::HoldPickupShelves->new, $c->param('hold_pickup_shelf_id') );
+
+    return $c->render_resource_not_found("Hold pickup shelf")
+        unless $hold_pickup_shelf;
+
+    return try {
+        $hold_pickup_shelf->delete;
+        return $c->render_resource_deleted;
+    } catch {
+        if ( blessed($_) && ref($_) eq 'Koha::Exceptions::Object::FKConstraintDeletion' ) {
+            return $c->render(
+                status  => 409,
+                openapi => {
+                    error      => 'Cannot delete hold pickup shelf',
+                    error_code => 'cannot_delete_used',
+                }
+            );
+        }
+        $c->unhandled_exception($_);
+    };
+}
+
+1;
