aab7298b4a21b68020f4d1d2ee9ae9cd381e7bbd
Johanna Räisä <johanna.raisa@gmail.com>
2025-03-21 13:28:32 +0200

    Add to waiting holds page

--- Koha/Hold.pm
+++ Koha/Hold.pm
@@ -37,6 +37,7 @@ use Koha::Libraries;
 use Koha::Old::Holds;
 use Koha::Calendar;
 use Koha::Plugins;
+use Koha::HoldPickupShelf;
 
 use Koha::BackgroundJob::BatchUpdateBiblioHoldsQueue;
 
@@ -611,6 +612,19 @@ sub desk {
     my $desk_rs = $self->_result->desk;
     return unless $desk_rs;
     return Koha::Desk->_new_from_dbic($desk_rs);
+}
+
+=head3 hold_pickup_shelf
+
+Returns the related Koha::HoldPickupShelf object for this Hold
+
+=cut
+
+sub hold_pickup_shelf {
+    my $self = shift;
+    my $rs   = $self->_result->hold_pickup_shelf;
+    return unless $rs;
+    return Koha::HoldPickupShelf->_new_from_dbic($rs);
 }
 
 =head3 borrower
